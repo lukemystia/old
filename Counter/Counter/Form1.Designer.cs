3db3df64c4d82d71b753c3128a321ae23d6e3927
lukemystia <komeizi.satori@gmail.com>
2016-08-14 12:01:13 +0900

    複数カウンタ
    カウンタを5つ

---
+++ Counter/Counter/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace Counter
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxCount1Name = new System.Windows.Forms.TextBox();
+			this.textBoxCount2Name = new System.Windows.Forms.TextBox();
+			this.textBoxCount3Name = new System.Windows.Forms.TextBox();
+			this.textBoxCount4Name = new System.Windows.Forms.TextBox();
+			this.textBoxCount5Name = new System.Windows.Forms.TextBox();
+			this.buttonCount1Plus = new System.Windows.Forms.Button();
+			this.buttonCount1Minus = new System.Windows.Forms.Button();
+			this.textBoxCount1Show = new System.Windows.Forms.TextBox();
+			this.textBoxCount2Show = new System.Windows.Forms.TextBox();
+			this.textBoxCount3Show = new System.Windows.Forms.TextBox();
+			this.textBoxCount4Show = new System.Windows.Forms.TextBox();
+			this.textBoxCount5Show = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.buttonSave = new System.Windows.Forms.Button();
+			this.buttonCount1Zero = new System.Windows.Forms.Button();
+			this.button13 = new System.Windows.Forms.Button();
+			this.button14 = new System.Windows.Forms.Button();
+			this.button15 = new System.Windows.Forms.Button();
+			this.button16 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBoxCount1Name
+			// 
+			this.textBoxCount1Name.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount1Name.Location = new System.Drawing.Point(13, 13);
+			this.textBoxCount1Name.Name = "textBoxCount1Name";
+			this.textBoxCount1Name.Size = new System.Drawing.Size(100, 23);
+			this.textBoxCount1Name.TabIndex = 0;
+			this.textBoxCount1Name.Text = "カウント1";
+			this.textBoxCount1Name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBoxCount2Name
+			// 
+			this.textBoxCount2Name.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount2Name.Location = new System.Drawing.Point(12, 42);
+			this.textBoxCount2Name.Name = "textBoxCount2Name";
+			this.textBoxCount2Name.Size = new System.Drawing.Size(100, 23);
+			this.textBoxCount2Name.TabIndex = 0;
+			this.textBoxCount2Name.Text = "カウント2";
+			this.textBoxCount2Name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBoxCount3Name
+			// 
+			this.textBoxCount3Name.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount3Name.Location = new System.Drawing.Point(12, 71);
+			this.textBoxCount3Name.Name = "textBoxCount3Name";
+			this.textBoxCount3Name.Size = new System.Drawing.Size(100, 23);
+			this.textBoxCount3Name.TabIndex = 0;
+			this.textBoxCount3Name.Text = "カウント3";
+			this.textBoxCount3Name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBoxCount4Name
+			// 
+			this.textBoxCount4Name.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount4Name.Location = new System.Drawing.Point(12, 100);
+			this.textBoxCount4Name.Name = "textBoxCount4Name";
+			this.textBoxCount4Name.Size = new System.Drawing.Size(100, 23);
+			this.textBoxCount4Name.TabIndex = 0;
+			this.textBoxCount4Name.Text = "カウント4";
+			this.textBoxCount4Name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBoxCount5Name
+			// 
+			this.textBoxCount5Name.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount5Name.Location = new System.Drawing.Point(12, 129);
+			this.textBoxCount5Name.Name = "textBoxCount5Name";
+			this.textBoxCount5Name.Size = new System.Drawing.Size(100, 23);
+			this.textBoxCount5Name.TabIndex = 0;
+			this.textBoxCount5Name.Text = "カウント5";
+			this.textBoxCount5Name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// buttonCount1Plus
+			// 
+			this.buttonCount1Plus.Location = new System.Drawing.Point(186, 12);
+			this.buttonCount1Plus.Name = "buttonCount1Plus";
+			this.buttonCount1Plus.Size = new System.Drawing.Size(39, 23);
+			this.buttonCount1Plus.TabIndex = 1;
+			this.buttonCount1Plus.Text = "+";
+			this.buttonCount1Plus.UseVisualStyleBackColor = true;
+			this.buttonCount1Plus.Click += new System.EventHandler(this.buttonCount1Plus_Click);
+			// 
+			// buttonCount1Minus
+			// 
+			this.buttonCount1Minus.Location = new System.Drawing.Point(231, 12);
+			this.buttonCount1Minus.Name = "buttonCount1Minus";
+			this.buttonCount1Minus.Size = new System.Drawing.Size(39, 23);
+			this.buttonCount1Minus.TabIndex = 1;
+			this.buttonCount1Minus.Text = "-";
+			this.buttonCount1Minus.UseVisualStyleBackColor = true;
+			this.buttonCount1Minus.Click += new System.EventHandler(this.buttonCount1Minus_Click);
+			// 
+			// textBoxCount1Show
+			// 
+			this.textBoxCount1Show.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount1Show.Location = new System.Drawing.Point(127, 13);
+			this.textBoxCount1Show.Name = "textBoxCount1Show";
+			this.textBoxCount1Show.ReadOnly = true;
+			this.textBoxCount1Show.Size = new System.Drawing.Size(44, 23);
+			this.textBoxCount1Show.TabIndex = 2;
+			this.textBoxCount1Show.Text = "0";
+			this.textBoxCount1Show.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBoxCount2Show
+			// 
+			this.textBoxCount2Show.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount2Show.Location = new System.Drawing.Point(127, 42);
+			this.textBoxCount2Show.Name = "textBoxCount2Show";
+			this.textBoxCount2Show.ReadOnly = true;
+			this.textBoxCount2Show.Size = new System.Drawing.Size(44, 23);
+			this.textBoxCount2Show.TabIndex = 2;
+			this.textBoxCount2Show.Text = "0";
+			this.textBoxCount2Show.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBoxCount3Show
+			// 
+			this.textBoxCount3Show.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount3Show.Location = new System.Drawing.Point(127, 71);
+			this.textBoxCount3Show.Name = "textBoxCount3Show";
+			this.textBoxCount3Show.ReadOnly = true;
+			this.textBoxCount3Show.Size = new System.Drawing.Size(44, 23);
+			this.textBoxCount3Show.TabIndex = 2;
+			this.textBoxCount3Show.Text = "0";
+			this.textBoxCount3Show.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBoxCount4Show
+			// 
+			this.textBoxCount4Show.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount4Show.Location = new System.Drawing.Point(127, 100);
+			this.textBoxCount4Show.Name = "textBoxCount4Show";
+			this.textBoxCount4Show.ReadOnly = true;
+			this.textBoxCount4Show.Size = new System.Drawing.Size(44, 23);
+			this.textBoxCount4Show.TabIndex = 2;
+			this.textBoxCount4Show.Text = "0";
+			this.textBoxCount4Show.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBoxCount5Show
+			// 
+			this.textBoxCount5Show.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBoxCount5Show.Location = new System.Drawing.Point(127, 129);
+			this.textBoxCount5Show.Name = "textBoxCount5Show";
+			this.textBoxCount5Show.ReadOnly = true;
+			this.textBoxCount5Show.Size = new System.Drawing.Size(44, 23);
+			this.textBoxCount5Show.TabIndex = 2;
+			this.textBoxCount5Show.Text = "0";
+			this.textBoxCount5Show.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(186, 42);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(39, 23);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "+";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(231, 42);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(39, 23);
+			this.button4.TabIndex = 1;
+			this.button4.Text = "-";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(186, 71);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(39, 23);
+			this.button5.TabIndex = 1;
+			this.button5.Text = "+";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(231, 71);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(39, 23);
+			this.button6.TabIndex = 1;
+			this.button6.Text = "-";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(186, 100);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(39, 23);
+			this.button7.TabIndex = 1;
+			this.button7.Text = "+";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(231, 100);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(39, 23);
+			this.button8.TabIndex = 1;
+			this.button8.Text = "-";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(186, 129);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(39, 23);
+			this.button9.TabIndex = 1;
+			this.button9.Text = "+";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(231, 129);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(39, 23);
+			this.button10.TabIndex = 1;
+			this.button10.Text = "-";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// buttonSave
+			// 
+			this.buttonSave.Location = new System.Drawing.Point(227, 158);
+			this.buttonSave.Name = "buttonSave";
+			this.buttonSave.Size = new System.Drawing.Size(88, 23);
+			this.buttonSave.TabIndex = 3;
+			this.buttonSave.Text = "保存";
+			this.buttonSave.UseVisualStyleBackColor = true;
+			this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+			// 
+			// buttonCount1Zero
+			// 
+			this.buttonCount1Zero.Location = new System.Drawing.Point(276, 12);
+			this.buttonCount1Zero.Name = "buttonCount1Zero";
+			this.buttonCount1Zero.Size = new System.Drawing.Size(39, 23);
+			this.buttonCount1Zero.TabIndex = 1;
+			this.buttonCount1Zero.Text = "0";
+			this.buttonCount1Zero.UseVisualStyleBackColor = true;
+			this.buttonCount1Zero.Click += new System.EventHandler(this.buttonCount1Zero_Click);
+			// 
+			// button13
+			// 
+			this.button13.Location = new System.Drawing.Point(276, 41);
+			this.button13.Name = "button13";
+			this.button13.Size = new System.Drawing.Size(39, 23);
+			this.button13.TabIndex = 1;
+			this.button13.Text = "0";
+			this.button13.UseVisualStyleBackColor = true;
+			this.button13.Click += new System.EventHandler(this.button13_Click);
+			// 
+			// button14
+			// 
+			this.button14.Location = new System.Drawing.Point(276, 70);
+			this.button14.Name = "button14";
+			this.button14.Size = new System.Drawing.Size(39, 23);
+			this.button14.TabIndex = 1;
+			this.button14.Text = "0";
+			this.button14.UseVisualStyleBackColor = true;
+			this.button14.Click += new System.EventHandler(this.button14_Click);
+			// 
+			// button15
+			// 
+			this.button15.Location = new System.Drawing.Point(276, 99);
+			this.button15.Name = "button15";
+			this.button15.Size = new System.Drawing.Size(39, 23);
+			this.button15.TabIndex = 1;
+			this.button15.Text = "0";
+			this.button15.UseVisualStyleBackColor = true;
+			this.button15.Click += new System.EventHandler(this.button15_Click);
+			// 
+			// button16
+			// 
+			this.button16.Location = new System.Drawing.Point(276, 129);
+			this.button16.Name = "button16";
+			this.button16.Size = new System.Drawing.Size(39, 23);
+			this.button16.TabIndex = 1;
+			this.button16.Text = "0";
+			this.button16.UseVisualStyleBackColor = true;
+			this.button16.Click += new System.EventHandler(this.button16_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(326, 187);
+			this.Controls.Add(this.buttonSave);
+			this.Controls.Add(this.textBoxCount5Show);
+			this.Controls.Add(this.textBoxCount4Show);
+			this.Controls.Add(this.textBoxCount3Show);
+			this.Controls.Add(this.textBoxCount2Show);
+			this.Controls.Add(this.textBoxCount1Show);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button16);
+			this.Controls.Add(this.button15);
+			this.Controls.Add(this.button14);
+			this.Controls.Add(this.button13);
+			this.Controls.Add(this.buttonCount1Zero);
+			this.Controls.Add(this.buttonCount1Minus);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.buttonCount1Plus);
+			this.Controls.Add(this.textBoxCount5Name);
+			this.Controls.Add(this.textBoxCount4Name);
+			this.Controls.Add(this.textBoxCount3Name);
+			this.Controls.Add(this.textBoxCount2Name);
+			this.Controls.Add(this.textBoxCount1Name);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "カウンター";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxCount1Name;
+		private System.Windows.Forms.TextBox textBoxCount2Name;
+		private System.Windows.Forms.TextBox textBoxCount3Name;
+		private System.Windows.Forms.TextBox textBoxCount4Name;
+		private System.Windows.Forms.TextBox textBoxCount5Name;
+		private System.Windows.Forms.Button buttonCount1Plus;
+		private System.Windows.Forms.Button buttonCount1Minus;
+		private System.Windows.Forms.TextBox textBoxCount1Show;
+		private System.Windows.Forms.TextBox textBoxCount2Show;
+		private System.Windows.Forms.TextBox textBoxCount3Show;
+		private System.Windows.Forms.TextBox textBoxCount4Show;
+		private System.Windows.Forms.TextBox textBoxCount5Show;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button buttonSave;
+		private System.Windows.Forms.Button buttonCount1Zero;
+		private System.Windows.Forms.Button button13;
+		private System.Windows.Forms.Button button14;
+		private System.Windows.Forms.Button button15;
+		private System.Windows.Forms.Button button16;
+	}
+}
+
---
+++ Counter/Counter/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace Counter
+{
+	public partial class Form1 : Form
+	{
+		private struct Counter 
+		{
+			public string name;
+			public int countNum;
+		}
+
+		//カウント変数
+		Counter[] count = new Counter[6];
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		//ロード時の処理
+		//保存データを読み込み反映
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			string line = "";
+			ArrayList al = new ArrayList();
+
+			using (StreamReader sr = new StreamReader("savedata", Encoding.GetEncoding("Shift_JIS")))
+			{
+				while ((line = sr.ReadLine()) != null)
+				{
+					al.Add(line);
+				}
+			}
+
+			count[1].name = al[0].ToString();
+			count[1].countNum = Convert.ToInt32(al[1]);
+
+			count[2].name = al[2].ToString();
+			count[2].countNum = Convert.ToInt32(al[3]);
+
+			count[3].name = al[4].ToString();
+			count[3].countNum = Convert.ToInt32(al[5]);
+
+			count[4].name = al[6].ToString();
+			count[4].countNum = Convert.ToInt32(al[7]);
+
+			count[5].name = al[8].ToString();
+			count[5].countNum = Convert.ToInt32(al[9]);
+
+
+			textBoxCount1Name.Text = count[1].name;
+			textBoxCount1Show.Text = count[1].countNum.ToString();
+
+			textBoxCount2Name.Text = count[2].name;
+			textBoxCount2Show.Text = count[2].countNum.ToString();
+
+			textBoxCount3Name.Text = count[3].name;
+			textBoxCount3Show.Text = count[3].countNum.ToString();
+
+			textBoxCount4Name.Text = count[4].name;
+			textBoxCount4Show.Text = count[4].countNum.ToString();
+
+			textBoxCount5Name.Text = count[5].name;
+			textBoxCount5Show.Text = count[5].countNum.ToString();
+		}
+
+		#region カウントボタン
+		#region カウント1
+		//+ボタン
+		private void buttonCount1Plus_Click(object sender, EventArgs e)
+		{
+			count[1].countNum++;
+			textBoxCount1Show.Text = count[1].countNum.ToString();
+		}
+
+		//-ボタン
+		private void buttonCount1Minus_Click(object sender, EventArgs e)
+		{
+			count[1].countNum--;
+			textBoxCount1Show.Text = count[1].countNum.ToString();
+		}
+
+		//0ボタン
+		private void buttonCount1Zero_Click(object sender, EventArgs e)
+		{
+			count[1].countNum = 0;
+			textBoxCount1Show.Text = count[1].countNum.ToString();
+		}
+		#endregion カウント1
+
+		#region カウント2
+		private void button3_Click(object sender, EventArgs e)
+		{
+			count[2].countNum++;
+			textBoxCount2Show.Text = count[2].countNum.ToString();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			count[2].countNum--;
+			textBoxCount2Show.Text = count[2].countNum.ToString();
+		}
+
+		private void button13_Click(object sender, EventArgs e)
+		{
+			count[2].countNum = 0;
+			textBoxCount2Show.Text = count[2].countNum.ToString();
+		}
+		#endregion カウント2
+
+		#region カウント3
+		private void button5_Click(object sender, EventArgs e)
+		{
+			count[3].countNum++;
+			textBoxCount3Show.Text = count[3].countNum.ToString();
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			count[3].countNum--;
+			textBoxCount3Show.Text = count[3].countNum.ToString();
+		}
+
+		private void button14_Click(object sender, EventArgs e)
+		{
+			count[3].countNum = 0;
+			textBoxCount3Show.Text = count[3].countNum.ToString();
+		}
+		#endregion カウント3
+
+		#region カウント4
+		private void button7_Click(object sender, EventArgs e)
+		{
+			count[4].countNum++;
+			textBoxCount4Show.Text = count[4].countNum.ToString();
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			count[4].countNum--;
+			textBoxCount4Show.Text = count[4].countNum.ToString();
+
+		}
+
+		private void button15_Click(object sender, EventArgs e)
+		{
+			count[4].countNum = 0;
+			textBoxCount4Show.Text = count[4].countNum.ToString();
+		}
+		#endregion カウント4
+
+		#region カウント5
+		private void button9_Click(object sender, EventArgs e)
+		{
+			count[5].countNum++;
+			textBoxCount5Show.Text = count[5].countNum.ToString();
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			count[5].countNum--;
+			textBoxCount5Show.Text = count[5].countNum.ToString();
+		}
+
+		private void button16_Click(object sender, EventArgs e)
+		{
+			count[5].countNum = 0;
+			textBoxCount5Show.Text = count[5].countNum.ToString();
+		}
+		#endregion カウント5
+		#endregion カウントボタン
+
+		//保存ボタン
+		private void buttonSave_Click(object sender, EventArgs e)
+		{
+			//前データ削除
+			System.IO.File.Delete("savedata");
+
+			Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+			StreamWriter writer = new StreamWriter("savedata", true, sjisEnc);
+			
+			writer.WriteLine(textBoxCount1Name.Text);
+			writer.WriteLine(textBoxCount1Show.Text);
+			writer.WriteLine(textBoxCount2Name.Text);
+			writer.WriteLine(textBoxCount2Show.Text);
+			writer.WriteLine(textBoxCount3Name.Text);
+			writer.WriteLine(textBoxCount3Show.Text);
+			writer.WriteLine(textBoxCount4Name.Text);
+			writer.WriteLine(textBoxCount4Show.Text);
+			writer.WriteLine(textBoxCount5Name.Text);
+			writer.WriteLine(textBoxCount5Show.Text);
+			
+			writer.Close();
+		}
+	}
+}
