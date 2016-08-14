c2931034565b5de68dd2d44a157afea7b781acba
lukemystia <komeizi.satori@gmail.com>
2016-08-14 12:09:36 +0900

    クリップボードビューア
    フリーソフトにもっといいものあるのを知った当時

---
+++ ClipboardViewer/ClipboardViewer/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ClipboardViewer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox
+			// 
+			this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox.Location = new System.Drawing.Point(0, 0);
+			this.textBox.Multiline = true;
+			this.textBox.Name = "textBox";
+			this.textBox.ReadOnly = true;
+			this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox.Size = new System.Drawing.Size(386, 245);
+			this.textBox.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox2.Location = new System.Drawing.Point(0, 284);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox2.Size = new System.Drawing.Size(386, 250);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(4, 269);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(54, 12);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "ひとつ前の";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(384, 531);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.ShowInTaskbar = false;
+			this.Text = "くりっぷぼーどびゅーわ";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ ClipboardViewer/ClipboardViewer/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClipboardViewer
+{
+	public partial class Form1 : Form
+	{
+		private MyClipboardViewer viewer;
+		private bool flag = false;
+
+		public Form1()
+		{
+			viewer = new MyClipboardViewer(this);
+
+			//イベントハンドラを登録
+			viewer.ClipboardHandler += this.OnClipBoardChanged;
+			InitializeComponent();
+		}
+
+		//クリップボードにテキストがコピーされると呼び出される
+		private void OnClipBoardChanged(object sender, ClipboardEventArgs args)
+		{
+			if (flag == true)
+			{
+				this.textBox2.Text = this.textBox.Text;
+				this.textBox.Text = args.Text;
+			}
+		}
+
+		//初期設定
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.textBox.Text = "クリップボードが変更されるたびに更新されます";
+			flag = true;
+		}
+	}
+}
